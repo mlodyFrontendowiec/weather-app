c71f3cc772dde42e413768d2599cc3bfeb42e75a
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-17 10:48:08 +0200

    Add constans

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API_URL } from "./constans";
 
 function App() {
-  return <div className="App"></div>;
+  const [city, setCity] = useState("");
+  const [data, setData] = useState("");
+
+  const fetchWeather = async (city) => {
+    await fetch(`${API_URL}${city}`)
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    fetchWeather("Warszawa");
+  }, []);
+
+  const handleInput = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWeather(city);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={city} onChange={handleInput} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
